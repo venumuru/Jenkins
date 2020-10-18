6d5158e2ff9404576022c371b18ebecfa34ba886
venumuru <46070899+venumuru@users.noreply.github.com>
2020-10-18 11:11:22 +0530

    Updated for getting latest pull from git

--- GitSample.java
+++ GitSample.java
@@ -2,6 +2,6 @@ public class GitSample
 {  
 public static void main(String args[])  
 {  
-System.out.println("1st program from jenkins");  
+System.out.println("2nd program from jenkins");  
 }  
 }  
